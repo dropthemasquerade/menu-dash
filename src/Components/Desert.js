d08d0c36826db561191eeea2dab29bfdc8c962d1
dropthemasquerade <dropthemasquerade@gmail.com>
2023-02-19 10:05:38 +0800

    add api

--- src/Components/Desert.js
+++ src/Components/Desert.js
@@ -5,8 +5,43 @@ import cards from "../img/card2.jpg";
 import PaymentSect from "./PaymentSect";
 import { Link } from "react-router-dom";
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const apiHost = ""; // 保持与页面服务器同一地址（nginx代理)
 
 function Desert() {
+
+    const [queryData, setQueryData] = useState([]);
+
+    const fetchData = async (data) => {
+        try {
+            // 提交到服务器
+            axios.get(apiHost + "/v1/client/pos/category", data,{ withCredentials: false }).then(
+                res => {
+                    console.log("progress return  ==>", res.data)
+                    const resp = res.data
+                    setQueryData(resp)
+                }
+            ).catch(err => {
+                console.log("err is->", err)
+            })
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        const data = {
+            "status": true // 初始化状态为1
+        }
+        fetchData(data).then(r => {
+            console.log("sss")
+        })
+    }, []);
+
     return (
         <>
             <div className="foodcontainer">
@@ -14,30 +49,15 @@ function Desert() {
                     <div className="cards">
                         <div className="all">
                             <div className="varieties">
-                                <Link to="/" className="var-btn">
-                                推荐
-                                </Link>
-                                <Link to="/african" className="var-btn">
-                                饮料
-                                </Link>
-                                <Link to="/chinese" className="var-btn">
-                                分类
-                                </Link>
-                                <Link to="/italian" className="var-btn">
-                                套餐
-                                </Link>
-                                <Link to="/desert" className="var-btn">
-                                卤菜
-                                </Link>
-                                <Link to="/desert" className="var-btn">
-                                配菜
-                                </Link>
-                                <Link to="/desert" className="var-btn">
-                                特色
-                                </Link>
-                                <Link to="/desert" className="var-btn">
-                                饮料
-                                </Link>
+                            {
+                                queryData.map(m => {
+                                    return (
+                                        <Link to={m.link} className="var-btn">
+                                            {m.name}
+                                        </Link>
+                                    )
+                                    })
+                                }
                             </div>
                         </div>
 
