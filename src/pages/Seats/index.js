5926639c55d09f979f909a41f60bf13b50c1d86d
dropthemasquerade <dropthemasquerade@gmail.com>
2023-02-25 00:16:26 +0800

    add top bap

--- src/pages/Seats/index.js
+++ src/pages/Seats/index.js
@@ -2,23 +2,14 @@ import React from "react";
 
 import "../../Components/seatbox.css";
 import "../../Components/container.css";
-import SeatBox from "../../Components/SeatBox";
-// import { Link } from "react-router-dom";
-import ResponsiveAppBar from "../../Components/AppBar";
-import ButtonAppBar from "../../Components/AppBar2";
-import SimpleBottomNavigation from "../../Components/BottomBar";
+
 import TopBar from "../../Components/TopBar";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
 
-import GrainIcon from '@mui/icons-material/Grain';
-import MessageIcon from '@mui/icons-material/Message';
-import AddLocationIcon from '@mui/icons-material/AddLocation';
 import MenuBox from "../../Components/MenuBox";
 
 const apiHost = ""; // 保持与页面服务器同一地址（nginx代理)
@@ -27,8 +18,6 @@ const apiHost = ""; // 保持与页面服务器同一地址（nginx代理)
 
 function SeatsShow() {
 
-    // const [updateCart, setUpdateCart] = useState(1);
-    // const [queryData, setQueryData] = useState([]);
     const [querySeat, setSeatData] = useState([]);
     const [themeMode, setThemeMode] = useState(false);
 
@@ -58,10 +47,6 @@ function SeatsShow() {
         fetchData(data).then(r => {
             console.log("sss")
         })
-
-        // fetchData2(data).then(r => {
-        //     console.log("sss2")
-        // })
 
     }, []);
     const clickedForUpdate = (e) => {
